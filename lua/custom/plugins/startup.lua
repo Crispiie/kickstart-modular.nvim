1d07bbbb45ce5e0500492c52e4e1e5985e221be5
Crispiie <austinbunn1@icloud.com>
2024-09-06 02:37:38 -0500

    Added leetcode.nvim
    leetcode.nvim is a neovim tool that allows you to use leetcode in the
    superior text editor

--- lua/custom/plugins/startup.lua
+++ lua/custom/plugins/startup.lua
@@ -3,6 +3,6 @@ return {
   requires = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
   config = function()
     -- THERE ARE 3 THEMES dashboard, evil, startify
-    require('startup').setup(require 'custom.startup.naruto_startup')
+    require('startup').setup(require 'custom.startup.lain')
   end,
 }
