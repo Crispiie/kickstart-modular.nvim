efbd940dbdf8efd7a4a726dc3812a0aac2d2f16b
Crispiie <austinbunn1@icloud.com>
2025-04-24 16:42:34 -0500

    Added suda.lua
    Suda is a neovim plugin that allows you to write in sudo while in a
    normal neovim session (ex. you nvim into a file you do not have read or
    write permissions and want to save your changes? Do :SudaWrite or
    :SudaRead

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -197,16 +197,10 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua',
         'clang-format',
-        'codelldb',
-        'clangd',
         'gdtoolkit',
         'bash-language-server',
         'lua-language-server',
-        'csharpier',
-        'omnisharp_mono',
-        'omnisharp',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
