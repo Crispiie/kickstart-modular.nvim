f9eff869af5602b5ccdb5eb7ee8afdc4d9782aca
crispi <austinbunn1@icloud.com>
2025-05-04 14:23:47 -0500

    Changed Required LSPs
    Removed clang-format from the required LSP's list

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -197,7 +197,6 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'clang-format',
         'gdtoolkit',
         'bash-language-server',
         'lua-language-server',
